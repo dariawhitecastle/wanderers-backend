d57f0db32eb985cf3658327e88c230fc453bd038
DariaDesiderata <daria.calwell@gmail.com>
2017-06-06 21:01:35 -0600

    static server

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const path = require('path')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const helmet = require('helmet')
+// const PORT = process.env.PORT || 3000
+
+// route files
+// const main = require('./routes/main')
+
+const PORT = process.env.PORT || 3000
+const app = express()
+
+app.use('/', express.static(path.join(__dirname, '../Wanderers/public')))
+
+app.use(cors({
+  origin: ['http://localhost:8080'],
+  credentials: false
+}))
+app.use(helmet())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// Routes
+// app.use('/', main)
+
+// Listening port
+app.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}`)
+})
---
+++ routes/main.js
@@ -0,0 +1,4 @@
+const router = require('express').Router()
+const knex = require('../db/knex')
+
+module.exports = router
